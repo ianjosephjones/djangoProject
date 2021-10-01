0063f011910026e25d73207dc465f669f5ad7719
ianjosephjones <64451104+ianjosephjones@users.noreply.github.com>
2021-10-01 13:05:03 -0400

    Render data Query data

--- base/models.py
+++ base/models.py
@@ -1,19 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.deletion import CASCADE
 # Create your models here.
 
 
 class Topic(models.Model):
     name = models.CharField(max_length=200)
 
-
-def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
 
 
 class Room(models.Model):
-    # host =
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
@@ -25,7 +23,7 @@ class Room(models.Model):
         return self.name
 
 
-class message(models.Model):
+class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     body = models.TextField()
