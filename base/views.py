385029801c908998680eff755130b397b1109fda
ianjosephjones <64451104+ianjosephjones@users.noreply.github.com>
2021-10-01 11:38:54 -0400

    Pull data and interpulating it the the home.html

--- base/views.py
+++ base/views.py
@@ -2,9 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 
+rooms = [
+    {'id': 1, 'name': 'Ian Jones'},
+    {'id': 2, 'name': 'Zach Jones'},
+    {'id': 3, 'name': 'Micah Jones'},
+
+]
+
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'rooms': rooms})
 
 
 def room(request):
