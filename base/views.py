79ab14e56b01263fedb95fbfad82da17351808e6
ianjosephjones <64451104+ianjosephjones@users.noreply.github.com>
2021-10-01 12:24:29 -0400

    Added class message w/ textField, ForeignKey, dateTimeField

--- base/views.py
+++ base/views.py
@@ -17,11 +17,7 @@ def home(request):
 
 
 def room(request, pk):
-
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
 
     return render(request, 'base/room.html', context)
