228a79bd4c3b0102f0e0997046a33450381df73b
ianjosephjones <64451104+ianjosephjones@users.noreply.github.com>
2021-10-01 12:29:46 -0400

    Added topic relationship

--- base/models.py
+++ base/models.py
@@ -4,9 +4,17 @@ from django.db.models.deletion import CASCADE
 # Create your models here.
 
 
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+
+def __str__(self):
+    return self.name
+
+
 class Room(models.Model):
     # host =
-    # topic =
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     # participant = models.
