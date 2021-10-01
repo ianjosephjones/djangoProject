31955076d2c768a72c50b31f632a9a93ec9a23ae
ianjosephjones <64451104+ianjosephjones@users.noreply.github.com>
2021-10-01 12:18:34 -0400

    Added the room data in the admin panel

--- base/views.py
+++ base/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
-
+from .models import Room
 # Create your views here.
 
-rooms = [
-    {'id': 1, 'name': 'Ian Jones'},
-    {'id': 2, 'name': 'Zach Jones'},
-    {'id': 3, 'name': 'Micah Jones'},
+# rooms = [
+#     {'id': 1, 'name': 'Ian Jones'},
+#     {'id': 2, 'name': 'Zach Jones'},
+#     {'id': 3, 'name': 'Micah Jones'},
 
-]
+# ]
 
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
