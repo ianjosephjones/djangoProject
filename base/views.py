6c408f1543cc46721c1739003df625d4bef429c4
ianjosephjones <64451104+ianjosephjones@users.noreply.github.com>
2021-10-04 15:12:37 -0400

    Added static css and js files

--- base/views.py
+++ base/views.py
@@ -20,29 +20,32 @@ from .forms import RoomForm
 
 
 def loginPage(request):
-    page = 'login'
-    if request.user.is_authenticated:
-        return redirect('home')
-
-    if request.method == 'POST':
-        username = request.POST.get('username').lower()
-        password = request.POST.get('password')
-
-        try:
-            user = User.objects.get(username=username)
-        except:
-            messages.error(request, 'User does not exist')
-
-        user = authenticate(request, username=username, password=password)
-
-        if user is not None:
-            login(request, user)
+    try:
+        page = 'login'
+        if request.user.is_authenticated:
             return redirect('home')
-        else:
-            messages.error(request, 'User OR password does not exist')
 
-    context = {'page': page}
-    return render(request, 'base/login_register.html', context)
+        if request.method == 'POST':
+            username = request.POST.get('username').lower()
+            password = request.POST.get('password')
+
+            try:
+                user = User.objects.get(username=username)
+            except:
+                messages.error(request, 'User does not exist')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.error(request, 'User OR password does not exist')
+
+        context = {'page': page}
+        return render(request, 'base/login_register.html', context)
+    except:
+        return render(request, 'base/404.html', context)
 
 
 def logoutUser(request):
