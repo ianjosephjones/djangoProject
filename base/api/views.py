20765efcaf934cc44dcb124ca0c2491563cc86d0
ianjosephjones <64451104+ianjosephjones@users.noreply.github.com>
2021-10-05 10:36:53 -0400

    serialized json data in rooms

--- base/api/views.py
+++ base/api/views.py
@@ -1,5 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from base.api import serializers
+from base.models import Room
+from .serializers import RoomcSerializer
 
 
 @api_view(['GET'])
@@ -10,3 +13,10 @@ def getRoutes(request):
         'GET /api/rooms/:id'
     ]
     return Response(routes)
+
+
+@api_view(['GET'])
+def getRooms(request):
+    rooms = Room.objects.all()
+    serializer = RoomcSerializer(rooms, many=True)
+    return Response(serializer.data)
