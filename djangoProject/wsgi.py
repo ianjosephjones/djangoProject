65f430386d2c326292abc096d31ae421488fbdcc
ianjosephjones <64451104+ianjosephjones@users.noreply.github.com>
2021-10-05 15:47:42 -0400

    test

--- djangoProject/wsgi.py
+++ djangoProject/wsgi.py
@@ -3,7 +3,7 @@ WSGI config for djangoProject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see some change1
+For more information on this file, see some change2
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
